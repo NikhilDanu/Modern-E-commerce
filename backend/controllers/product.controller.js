16d0607a6cb1dfee63918145d28ad315124f8c5b
NikhilDanu <ns2297877@gmail.com>
2026-04-21 12:27:32 +0530

    Update product.controller.js

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -89,3 +89,18 @@ exports.getAllTeams = async (req, res) => {
     res.status(500).json({ message: "Internal error" });
   }
 };
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error fetching product" });
+  }
+};
