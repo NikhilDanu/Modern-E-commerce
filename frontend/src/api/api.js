402bf536e43b87b32f3f0830e17884a7732140d8
NikhilDanu <ns2297877@gmail.com>
2026-04-21 12:36:30 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL:  "http://localhost:5000/api",
+  baseURL:  "https://modern-e-commerce.onrender.com/api",
 });
 
 API.interceptors.request.use(
